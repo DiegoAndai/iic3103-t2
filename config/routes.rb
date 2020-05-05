546d9149cbf7ba37c380a7028b9507767fa6f445
DiegoAndai <dfandai@uc.cl>
2020-05-04 20:26:46 -0400

    change routes from plural to singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :ingredientes, only: [:index, :show, :create, :update, :destroy]
-  resources :hamburguesas, only: [:index, :show, :create, :update, :destroy] do
-    put 'ingrediente/:ingrediente_id', to: 'hamburguesas#add_ingrediente'
-    delete 'ingrediente/:ingrediente_id', to: 'hamburguesas#delete_ingrediente'
+  resources :ingredientes,
+    only: [:index, :show, :create, :update, :destroy],
+    path: 'ingrediente'
+  resources :hamburguesas, only: [:index, :show, :create, :update, :destroy],
+    path: 'hamburguesa' do
+      put 'ingrediente/:ingrediente_id', to: 'hamburguesas#add_ingrediente'
+      delete 'ingrediente/:ingrediente_id', to: 'hamburguesas#delete_ingrediente'
   end
 end
