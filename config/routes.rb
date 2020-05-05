5269444f750841aaa34e9a8e7e661a1ce2e0f023
DiegoAndai <dfandai@uc.cl>
2020-05-04 21:12:17 -0400

    default to json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :ingredientes,
     only: [:index, :show, :create, :update, :destroy],
-    path: 'ingrediente'
+    path: 'ingrediente',
+    defaults: { format: 'json' }
   resources :hamburguesas, only: [:index, :show, :create, :update, :destroy],
-    path: 'hamburguesa' do
+    path: 'hamburguesa',
+    defaults: { format: 'json' } do
       put 'ingrediente/:ingrediente_id', to: 'hamburguesas#add_ingrediente'
       delete 'ingrediente/:ingrediente_id', to: 'hamburguesas#delete_ingrediente'
   end
